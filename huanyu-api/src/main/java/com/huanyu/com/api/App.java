213559df26e06ef26e69057f56e3b5e4c3550398
ares <yangtaocommon@163.com>
2017-02-26 18:25:32 +0800

    init

---
+++ huanyu-api/src/main/java/com/huanyu/com/api/App.java
@@ -0,0 +1,13 @@
+package com.huanyu.com.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
