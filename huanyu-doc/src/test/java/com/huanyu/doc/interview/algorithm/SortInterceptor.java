dd737e0468b3de04c4b6d56ec6b7b48f346ee84c
ares <yangtaocommon@163.com>
2017-09-01 10:01:27 +0800

    backup

--- huanyu-doc/src/test/java/com/huanyu/doc/interview/algorithm/SortInterceptor.java
+++ huanyu-doc/src/test/java/com/huanyu/doc/interview/algorithm/SortInterceptor.java
@@ -2,13 +2,12 @@ package com.huanyu.doc.interview.algorithm;
 
 import com.alibaba.fastjson.JSON;
 import com.huanyu.common.utils.PerfLogger;
+import java.lang.reflect.Method;
 import net.sf.cglib.proxy.Enhancer;
 import net.sf.cglib.proxy.MethodInterceptor;
 import net.sf.cglib.proxy.MethodProxy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.lang.reflect.Method;
 
 /**
  * 基于cglib 实现的切面
@@ -30,6 +29,7 @@ public class SortInterceptor implements MethodInterceptor {
 
   public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy)
     throws Throwable {
+    LOG.info("==========================================================");
     LOG.info("before sort,array:{}", JSON.toJSONString(args[0], true));
     //    PerfLogger pf = new PerfLogger(LOG).start("sort");
     PerfLogger pf = new PerfLogger(LOG)
@@ -38,6 +38,7 @@ public class SortInterceptor implements MethodInterceptor {
     Object o1 = proxy.invokeSuper(obj, args);
     pf.stop();
     LOG.info("after sort,array:{}", JSON.toJSONString(args[0], true));
+    LOG.info("**********************************************************");
     return o1;
   }
 
